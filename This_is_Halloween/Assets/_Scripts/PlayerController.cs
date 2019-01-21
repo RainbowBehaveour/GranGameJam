35d955107880b91f32dda00f2994ee560330ea8f
Alex Morales Garcia <36154498+AlexMG99@users.noreply.github.com>
2019-01-21 12:24:16 +0100

    Correcte some bugs

--- This_is_Halloween/Assets/_Scripts/PlayerController.cs
+++ This_is_Halloween/Assets/_Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float speed = 1.0f;
+    public float bullet_speed = 1.0f;
+
     public GameObject bullet;
     private Camera cam;
 
@@ -37,19 +39,22 @@ public class PlayerController : MonoBehaviour {
     {
         GameObject new_bullet = Instantiate(bullet, transform.position, transform.rotation);
 
-        Vector2 mousePos = Vector3.zero;
+        Vector2 mousePos = Vector2.zero;
         Vector3 point = Vector3.zero;
         Vector3 direction = Vector3.zero;
 
         mousePos.Set(Input.mousePosition.x, Input.mousePosition.y);
         point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, transform.position.z));
-        direction = point - transform.position;
+        direction.x = point.x - transform.position.x;
+        direction.y = point.y - transform.position.y;
+        direction.z = 0;
 
         direction.Normalize();
 
+
         Rigidbody2D rb = new_bullet.GetComponent<Rigidbody2D>();
 
-        rb.velocity = direction;
+        rb.velocity = direction * bullet_speed * Time.deltaTime;
     }
 
    
--- This_is_Halloween/Assets/_Scripts/ShootingMechanic.cs
+++ This_is_Halloween/Assets/_Scripts/ShootingMechanic.cs
@@ -4,18 +4,9 @@ using UnityEngine;
 
 public class ShootingMechanic : MonoBehaviour {
 
-    GameObject player;
-    Vector3 direction = Vector3.zero;
-
     // Use this for initialization
     void Start () {
-        
-    }
-	
-	// Update is called once per frame
-	void Update ()
-    {
-      
+       
 
     }
 }
