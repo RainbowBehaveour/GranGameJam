a303898eeba8445a8628290206f0b77ecceca33d
Alex Morales Garcia <36154498+AlexMG99@users.noreply.github.com>
2019-01-21 14:12:53 +0100

    Added Player Rotation

--- This_is_Halloween/Assets/_Scripts/PlayerController.cs
+++ This_is_Halloween/Assets/_Scripts/PlayerController.cs
@@ -6,9 +6,13 @@ public class PlayerController : MonoBehaviour {
 
     public float speed = 1.0f;
     public float bullet_speed = 1.0f;
+    public float max_health = 100.0f;
+    public float current_health = 100.0f;
+    public float damage = 1.0f;
 
     public GameObject bullet;
     private Camera cam;
+    Vector3 direction = Vector3.zero;
 
     // Use this for initialization
     void Start ()
@@ -26,37 +30,43 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = Vector3.zero;
         movement.Set(moveHorizontal, 0f, moveVertical);
 
+        CalculateDirection();
+        float rot_angle = (Mathf.Acos(direction.z) * 180) / Mathf.PI;
+
         transform.position += movement*speed;
+        transform.rotation = Quaternion.Euler(90, rot_angle, 0);
+
+       
 
         //Create Shoot
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            SetDirection();
+            CreateBullet();
         }
+
 	}
 
-    void SetDirection()
+    void CreateBullet()
     {
         GameObject new_bullet = Instantiate(bullet, transform.position, transform.rotation);
-
-        Vector2 mousePos = Vector2.zero;
-        Vector3 player_Pos = Vector3.zero;
-        Vector3 direction = Vector3.zero;
-
-        mousePos.Set(Input.mousePosition.x, Input.mousePosition.y);
-        player_Pos = cam.WorldToScreenPoint(transform.position);
-        Debug.Log(player_Pos);
-        Debug.Log(Input.mousePosition);
-        direction.x = Input.mousePosition.x - player_Pos.x;
-        direction.y = 0f;
-        direction.z = Input.mousePosition.y - player_Pos.y;
-
-        direction.Normalize();
-       // Debug.Log(direction);
         Rigidbody rb = new_bullet.GetComponent<Rigidbody>();
 
         rb.velocity = direction * bullet_speed;
     }
 
-   
+    //Calculate Direction between Mouse & Player owo
+   void CalculateDirection()
+    {
+        Vector2 mousePos = Vector2.zero;
+        Vector3 player_Pos = Vector3.zero;
+
+        mousePos.Set(Input.mousePosition.x, Input.mousePosition.y);
+        player_Pos = cam.WorldToScreenPoint(transform.position);
+
+        direction.x = Input.mousePosition.x - player_Pos.x;
+        direction.y = 0f;
+        direction.z = Input.mousePosition.y - player_Pos.y;
+        direction.Normalize();
+    }
+
 }
