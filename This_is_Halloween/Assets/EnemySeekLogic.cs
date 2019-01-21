199b5b7a4b552c0b2fe24e85cee4331d9086399e
Josep Pi <josep.pi.serra@gmail.com>
2019-01-21 13:59:14 +0100

    Update enemies behaveour logic

--- This_is_Halloween/Assets/EnemySeekLogic.cs
+++ This_is_Halloween/Assets/EnemySeekLogic.cs
@@ -5,14 +5,21 @@ using NodeCanvas.Framework;
 
 public class EnemySeekLogic : MonoBehaviour {
     Blackboard my_board;
+    GameObject player;
 	// Use this for initialization
 	void Start () {
         my_board = GetComponent<Blackboard>();
+        player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        my_board["target"] = GameObject.FindGameObjectWithTag("Player").transform.position;
-        Debug.Log(GameObject.FindGameObjectWithTag("Player").transform.position);
+        Vector3 distance_from_player;
+        distance_from_player = player.transform.position - transform.position;
+        if (distance_from_player.magnitude <= GetComponent<SphereCollider>().radius)
+        {
+            my_board["target"] = GameObject.FindGameObjectWithTag("Player").transform.position;
+            Debug.Log(GameObject.FindGameObjectWithTag("Player").transform.position);
+        }
 	}
 }
