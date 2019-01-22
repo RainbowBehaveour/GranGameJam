4f0000a9fff419f9a3fe323b26898b6ea648165a
Yessica Servin Dominguez <yessica.servindominguez@gmail.com>
2019-01-22 14:47:35 +0100

    added bullet live

--- This_is_Halloween/Assets/_Scripts/ShootingMechanic.cs
+++ This_is_Halloween/Assets/_Scripts/ShootingMechanic.cs
@@ -7,16 +7,19 @@ public class ShootingMechanic : MonoBehaviour {
     // Use this for initialization
     Vector3 startPos = Vector3.zero;
     public float MaxDistance=8;
-
+    public float lives_Time = 5;
+    float startTime;
+    float currentTime;
     void Start () {
-        startPos = transform.position; 
-
+        startPos = transform.position;
+        startTime = Time.fixedTime;
     }
 
     private void Update()
     {
         float actualDistance = Vector3.Distance(startPos, transform.position);
-       if(actualDistance >= MaxDistance)
+        currentTime = Time.fixedTime - startTime;
+       if(actualDistance >= MaxDistance || currentTime>= lives_Time)
         {
             Destroy(transform.gameObject);
         }
