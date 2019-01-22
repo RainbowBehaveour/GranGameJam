824fb8af0ced20cbed52b0e3ccdc874135a8fee1
Alex Morales Garcia <36154498+AlexMG99@users.noreply.github.com>
2019-01-22 14:34:01 +0100

    Added Scene Menu

---
+++ This_is_Halloween/Assets/_Scripts/ChangeScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+	public void ChangeSceneToGame()
+    {
+        SceneManager.LoadScene("AI Framework");
+    }
+}
