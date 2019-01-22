ca3d9626a3241f5f049b1d8e4dfdb9a2b677e837
Alex Morales Garcia <36154498+AlexMG99@users.noreply.github.com>
2019-01-22 11:23:36 +0100

    Added Dash Mechanic

--- This_is_Halloween/Assets/_Scripts/PlayerController.cs
+++ This_is_Halloween/Assets/_Scripts/PlayerController.cs
@@ -2,51 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+
+    public float dash_speed = 0.0005f;
+    float dash_time = 0.3f;
+    bool isDash = false;
+    float lastSpaceDown;
 
     public float speed = 1.0f;
-    public float dash_speed = 1.0f;
     public float rotation_speed = 0.05f;
+
     public Animator animator;
     private Camera cam;
+
     Vector3 direction = Vector3.zero;
     Vector3 player_screenPos = Vector3.zero;
 
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         cam = Camera.main;
-      
     }
-	
-  
-	// Update is called once per frame
-	void Update () {
+
+
+    // Update is called once per frame
+    void Update()
+    {
 
         //Movement
-		float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-		Vector3 movement = Vector3.zero;
+        Vector3 movement = Vector3.zero;
         movement.Set(moveHorizontal, 0f, moveVertical);
 
         CalculateDirection();
         float rot_angle = (Mathf.Acos(direction.z) * 180) / Mathf.PI;
-        
-        if(Input.mousePosition.x < player_screenPos.x)
+
+        if (Input.mousePosition.x < player_screenPos.x)
         {
             rot_angle = -rot_angle;
         }
 
         animator.SetFloat("Angle", rot_angle);
-        transform.position += transform.up*moveVertical * speed;
-        
+        transform.position += transform.up * moveVertical * speed;
+
         Quaternion mouse_quaternion = Quaternion.Euler(90, rot_angle, 0);
 
         transform.rotation = Quaternion.Lerp(transform.rotation, mouse_quaternion, rotation_speed);
 
-	}
+        if (Input.GetKeyDown(KeyCode.Space) && !isDash)
+        {
+            isDash = true;
+            lastSpaceDown = Time.fixedTime;
+        }
+
+        if (isDash)
+        {
+            float currentTime = Time.fixedTime - lastSpaceDown;
+            if (currentTime >= dash_time)
+            {
+                isDash = false;
+            }
+            transform.position += transform.up * dash_speed;
+        }
+    }
 
     //Calculate Direction between Mouse & Player owo
     void CalculateDirection()
@@ -59,8 +81,8 @@ public class PlayerController : MonoBehaviour {
         direction.x = Input.mousePosition.x - player_screenPos.x;
         direction.y = 0f;
         direction.z = Input.mousePosition.y - player_screenPos.y;
-        
+
         direction.Normalize();
     }
-
 }
+
